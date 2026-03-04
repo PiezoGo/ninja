2b8b4224cc8912f983dfa49fdd31e8752beee436
piento <piezo5go@gmail.com>
2026-03-04 09:59:29 +0300

    Synchornous and asynchronous function in flutter,,,, using the async and await keywords

--- worldtime/lib/pages/choose_location.dart
+++ worldtime/lib/pages/choose_location.dart
@@ -3,12 +3,29 @@ import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
-
+ 
   @override
   State<ChooseLocation> createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+
+  void getData() async {
+    String username = await Future.delayed(Duration(seconds: 3), () => 'Michael Ochieng' );
+
+    String bio = await Future.delayed(Duration(seconds: 2), () => 'Software Engineer from Multimedia university of Kenya!!' );
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
