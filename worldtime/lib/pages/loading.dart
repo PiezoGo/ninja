2f01eac2e857f97c5935ab4cdc5b201d619be5eb
piento <piezo5go@gmail.com>
2026-03-04 13:44:45 +0300

    teh get function is now working appropriately ,,, the branch is ready for a merge with the main branch

--- worldtime/lib/pages/loading.dart
+++ worldtime/lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'dart:convert';
 
 
 class Loading extends StatefulWidget {
@@ -12,11 +13,17 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
 void getData() async {
-  var url = Uri.https('https://time.now/developer/api/timezone/Europe/London');
+  var url = Uri.https('time.now', 'developer/api/timezone/Europe/London');
   
   Response resp = await get(url);
-  print(resp.body);
+  // print(resp.body);
 
+  Map data = jsonDecode(resp.body);
+  print(data);
+
+  String datetime = data['datetime'];
+  DateTime now = DateTime.parse(datetime);
+  print(now);
 
   }
 
